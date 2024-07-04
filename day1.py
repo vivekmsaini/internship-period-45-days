f210614e5bf967ac136088d8e80a29aaded4cb27
Vivek Saini <sainivivek2704@gmail.com>
2024-07-04 13:51:08 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,18 @@
+## genral introduction of python programing
+
+print("hello world")
+
+var1=True
+var2=False
+print(var1)
+print(var2)
+print(type(var1))
+print (type(var2))
+
+num1=100
+print(num1)
+print(type(num1))
+
+num1=100.45
+print(num1)
+print(type(num1))
